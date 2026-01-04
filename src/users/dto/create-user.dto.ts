b76c83a2d095691513304ea3f43727e4955c6cf9
Maksudr <maksudurrahamanmishu7@gmail.com>
2026-01-04 11:58:01 +0600

    create a route

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsString, IsEmail, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(8,{ message: 'Password is too short. Minimum length is $constraint1 characters.' })
+    password: string;
+
+    @IsString()
+    image?: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/lib/prisma.service';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService) { }
+
+  async createUser(createUserDto: CreateUserDto) {
+    try {
+      // Logic to create a user in the database
+      const newUser = await this.prisma.user.create({
+        data: {
+          name: createUserDto.name,
+          email: createUserDto.email,
+          password: createUserDto.password,
+          image: createUserDto.image,
+        }
+      })
+
+      return newUser;
+
+    } catch (error) {
+      const err = error as Error;
+      console.log(err.message);
+    }
   }
 
   findAll() {
